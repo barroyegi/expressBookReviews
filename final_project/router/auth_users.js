ab8db7501902d00975921b362cf39a901c2e0c09
barroyegi <barroyegi@gmail.com>
2025-06-04 05:31:07 -0400

    Added PUT and DELETE requests for adding and deleting reviews

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -48,9 +48,50 @@ regd_users.post("/login", (req, res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const review = req.body.review;
+    const username = req.session?.authorization?.username;
+
+    // Validaciones
+    if (!username) {
+        return res.status(401).json({ message: "You must sign in to write a review" });
+    }
+
+    if (!books[isbn]) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+
+    if (!review) {
+        return res.status(400).json({ message: "The review can not be empty" });
+    }
+
+    // Añadir o actualizar la reseña
+    books[isbn].reviews[username] = review;
+
+    return res.status(200).json({ message: "Review added." });
 });
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session?.authorization?.username;
+
+    if (!username) {
+        return res.status(401).json({ message: "NYou must sign in to write a review" });
+    }
+
+    if (!books[isbn]) {
+        return res.status(404).json({ message: "Book not found." });
+    }
+
+    if (!books[isbn].reviews[username]) {
+        return res.status(404).json({ message: "You don't have any reviews for this book." });
+    }
+
+    delete books[isbn].reviews[username];
+
+    return res.status(200).json({ message: "Review deleted successfuly." });
+});
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
