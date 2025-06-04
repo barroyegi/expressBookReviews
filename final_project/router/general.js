ddddcc831ce38ea2b55e772e5ee87ac4ea58509c
barroyegi <barroyegi@gmail.com>
2025-06-04 03:36:06 -0400

    Added GET request for getting the review based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -69,8 +69,15 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    const review = book.reviews;
+
+    if (book) {
+        res.json(review);
+    } else {
+        res.status(404).json({ message: 'Book not found'});
+    }
 });
 
 module.exports.general = public_users;
